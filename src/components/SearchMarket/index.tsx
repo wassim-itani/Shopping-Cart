f4e1bb3cd91827aed0f374561e58dc48c3a15d31
Wassim Itani <wassim.j.itani@gmail.com>
2023-03-21 12:55:45 +0200

    Implement live search

--- src/components/SearchMarket/index.tsx
+++ src/components/SearchMarket/index.tsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { searchItems } from "../../store/features/marketSlice";
+
 const SearchMarket = () => {
+  const [query, setQuery] = useState<string>("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(searchItems(query));
+  }, [query]);
+
+  const changeQuery = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setQuery(e.target.value);
+  };
+
   return (
     <input
       type="text"
+      value={query}
       className="w-72 py-1 px-2 text-gray-600 border-2 border-red-500 outline-0"
       placeholder="Search the market..."
+      onChange={changeQuery}
     />
   );
 };
